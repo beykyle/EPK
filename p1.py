2afa14cef240b79ecdf61390e8c7b513ef081569
beykyle <beykyle@umich.edu>
2021-03-24 16:46:41 -0400

    Fixed ramp time, added more time steps

--- p1.py
+++ p1.py
@@ -8,8 +8,8 @@ from solver import *
 test()
 
 # set up time grid
-timesteps = 100
-t = np.linspace(0,10,num=timesteps)
+timesteps = 1001
+t = np.linspace(0,6.0001,num=timesteps)
 
 # set up kinetics data
 precursor_groups = 1
@@ -24,7 +24,7 @@ d.lambda_precursor  = d.lambda_precursor * 0.49405
 data = Data.buildFromConstant(d, t)
 
 # build a reactivity ramp
-times = [0,1,5]
+times = [0,1,6]
 rho_ramp_up = LinearReactivityRamp(0,0.5 * d.beff, 1)
 rho_ramp_down = LinearReactivityRamp(0.5 * d.beff, 0, 5)
 rho = PieceWiseReactivityRamp(times , [rho_ramp_up, rho_ramp_down])
@@ -38,10 +38,12 @@ power_numeric = solver.p
 power_analytic = solver.analyticPower1DG()
 
 # plot reactivity ramp
-Plotter.plotReactivityRamp(rho)
+#plot_rx = Plotter(t)
+#plot_rx.plotReactivityRamp(rho)
+#plot_rx.save("./p1_rx.pdf")
 
 # compare analytic and numeric power with the plotter
 plotter = Plotter(t)
-plotter.addPower(power_analytic, "Analytic")
-plotter.addPower(power_numeric,  "EPKE Solver")
+plotter.addData(power_analytic, "Analytic")
+plotter.addData(power_numeric,  "EPKE Solver")
 plotter.save("./p1.pdf")
