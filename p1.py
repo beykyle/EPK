0f2f11807503ccafdeaf68aaab0f0c22ed0b19d3
beykyle <beykyle@umich.edu>
2021-03-25 14:43:07 -0400

    Changed reactivity specs

--- p1.py
+++ p1.py
@@ -12,11 +12,10 @@ timesteps = 1001
 t = np.linspace(0,6.0001,num=timesteps)
 
 # set up kinetics data
-precursor_groups = 1
-d = ConstantKineticsData(precursor_groups)
+d = ConstantKineticsData()
 d.beff = 0.76
 d.mgt = 2.6E-15
-d.lambda_precursor  = d.lambda_precursor * 0.49405
+d.lambda_precursor  = np.array([0.49405])
 
 # thermal feedback dynamics data
 
@@ -24,10 +23,10 @@ d.lambda_precursor  = d.lambda_precursor * 0.49405
 data = Data.buildFromConstant(d, t)
 
 # build a reactivity ramp
-times = [0,1,6]
-rho_ramp_up = LinearReactivityRamp(0,0.5 * d.beff, 1)
-rho_ramp_down = LinearReactivityRamp(0.5 * d.beff, 0, 5)
-rho = PieceWiseReactivityRamp(times , [rho_ramp_up, rho_ramp_down])
+times = [0,1,6] # total asym ramp time: 6s
+rho_ramp_up = LinearReactivityRamp(0,0.5 * d.beff, 1) # 0$ -> 0.5$ in 1s
+rho_ramp_down = LinearReactivityRamp(0.5 * d.beff, 0, 5) #0.5$ -> 0$ in 5 s
+rho = PieceWiseReactivityRamp(times , [rho_ramp_up, rho_ramp_down], t)
 
 # run the solver
 solver = Solver(data,t,rho)
@@ -38,12 +37,12 @@ power_numeric = solver.p
 power_analytic = solver.analyticPower1DG()
 
 # plot reactivity ramp
-#plot_rx = Plotter(t)
-#plot_rx.plotReactivityRamp(rho)
-#plot_rx.save("./p1_rx.pdf")
+plot_rx = Plotter(t, ylabel=r"$\rho$ [\$]")
+plot_rx.plotReactivityRamp(rho)
+plot_rx.save("./results/p1_rx.pdf")
 
 # compare analytic and numeric power with the plotter
 plotter = Plotter(t)
-plotter.addData(power_analytic, "Analytic")
-plotter.addData(power_numeric,  "EPKE Solver")
+plotter.addData(power_analytic, label="Analytic")
+plotter.addData(power_numeric,  label="EPKE Solver")
 plotter.save("./results/p1.pdf")
