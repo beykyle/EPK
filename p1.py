5b560ebf9245e0bd188d327a10aba7292f928001
deanrp <deanrp@highpoint.engin.umich.edu>
2021-03-26 01:24:44 -0400

    changed to working

--- p1.py
+++ p1.py
@@ -32,11 +32,13 @@ rho_ramp_down = LinearReactivityRamp(0.5 * d.beff, 0, 5) #0.5$ -> 0$ in 5 s
 rho = PieceWiseReactivityRamp(times , [rho_ramp_up, rho_ramp_down], t)
 
 # run the solver
-solver = Solver(data,t,rho, debug=True)
-data.gamma_D += 0#-1.2 #changing these for development
-data.lambda_H += 0#1 #changing these for development
-solver = Solver(data,t,rho, debug=True)
-solver.solve(0.5)
+solver = Solver(data,t,rho, debug=False)
+data.gamma_D += -1.2 #changing these for development
+data.lambda_H += 1 #changing these for development
+solver.solve(0.5, True)
+
+exit()
+
 
 power_numeric = solver.p
 # get the analytic solution
@@ -45,10 +47,11 @@ power_analytic = solver.analyticPower1DG()
 # plot reactivity ramp
 plot_rx = Plotter(t, ylabel=r"$\rho$ [\$]")
 plot_rx.plotReactivityRamp(rho, data.beff)
-plot_rx.save("./results/p1_rx.pdf")
+#plot_rx.save("./results/p1_rx.pdf")
 
 # compare analytic and numeric power with the plotter
 plotter = Plotter(t)
 plotter.addData(power_analytic, label="Analytic", marker="--")
 plotter.addData(power_numeric,  label="EPKE Solver", marker="x")
-plotter.save("./results/p1.pdf")
+#plotter.save("./results/p1.pdf")
+plt.show()
