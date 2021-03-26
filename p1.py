27de78acd582dbe7631f81a2f68c539127d31813
beykyle <beykyle@umich.edu>
2021-03-26 14:30:36 -0400

    Added timestep and finite differencing scheme comparisons. Explicit differencing fails

--- p1.py
+++ p1.py
@@ -12,7 +12,7 @@ Problem C.1 on 551 HW 4
 test()
 
 # set up time grid
-timesteps = 4200
+timesteps = 501
 t = np.linspace(0,6.0001,num=timesteps)
 
 # set up kinetics data
@@ -24,6 +24,8 @@ d.lambda_precursor  = np.array([0.49405])
 d.beff              = np.array([0.76])
 
 # thermal feedback dynamics data
+d.lambda_H = 0
+d.gamma_D = 0
 
 # build data object gridded over time steps
 data = Data.buildFromConstant(d, t)
@@ -34,19 +36,47 @@ rho_ramp_up = LinearReactivityRamp(0,0.5 * d.beff, 1) # 0$ -> 0.5$ in 1s
 rho_ramp_down = LinearReactivityRamp(0.5 * d.beff, 0, 5) #0.5$ -> 0$ in 5 s
 rho = PieceWiseReactivityRamp(times , [rho_ramp_up, rho_ramp_down], t)
 
-# compare feedback numerical w/ analytic
-solver = Solver(data,t,rho, debug=False)
-solver.solve(0.5,feedback=True)
-power_feed_solver = solver.p
+# set up plotter and solver, get analytic soln
+solver = Solver(data,t,rho)
 power_analytic = solver.analyticPower1DG()
+power_plotter = Plotter(t)
+
+# compare numerical w/ analytic, theta study
+solver.solve(0.5)
+power_plotter.addData(solver.p, label=r"EPK, $\theta=\frac{1}{2}$", marker=".")
+solver.solve(1)
+power_plotter.addData(solver.p, label=r"EPK, $\theta=1$", marker=".", alpha=0.5)
+#solver.debug = True
+solver.solve(0)
+power_plotter.addData(solver.p, label=r"EPK, $\theta=0$", marker=".", alpha=0.5)
+
+# plot analytic soln on top
+power_plotter.addData(power_analytic, label="Analytic", marker="--")
+
+# plot reactivity
+react_plotter = Plotter(t)
+react_plotter.plotReactivityRamp(rho, data.beff)
+
+# save figures
+power_plotter.save("./results/theta_study_nofeedback.pdf")
+react_plotter.save("./results/p1_rx.pdf")
+
+# time step study
+timesteps = [101, 201, 501, 1001, 2001, 5001, 10001]
+l2_diff = []
+for num_t in timesteps:
+    # make new time grid
+    t = np.linspace(0,6.0001,num=num_t)
+    # set up data and reactivity on this time grid
+    data = Data.buildFromConstant(d, t)
+    rho = PieceWiseReactivityRamp(times , [rho_ramp_up, rho_ramp_down], t)
+    # set up and run solver
+    solver = Solver(data,t,rho)
+    power_analytic = solver.analyticPower1DG()
+    solver.solve(0.5)
+    l2_diff.append(np.linalg.norm(power_analytic - solver.p))
 
 
-# plot analytic comparison
-p = Plotter(t)
-p.addData(power_analytic, label="Analytic", marker="--")
-p.addData(power_analytic, label="EPK", marker="x")
-p.save("./results/p1.pdf")
-
-p = Plotter(t, ylabel=r"$\rho$ [\$]")
-p.plotReactivityRamp(rho,data.beff)
-p.save("./results/p1_rx.pdf")
+p = Plotter(6.0/np.array(timesteps),  xlabel=r"time step [s]", ylabel=r"$L_2(p_{analytic}, p_{epk})$")
+p.addData(np.array(l2_diff))
+p.save("./results/time_step.pdf")
