9b9cf36faa1ed61ca95bb1b1d87d7c1874ee8a06
beykyle <beykyle@umich.edu>
2021-03-26 10:25:30 -0400

    Split problem code into multiple files, added back use of plotter class

--- p2.py
+++ p2.py
@@ -4,8 +4,9 @@ import numpy as np
 
 from solver import *
 import matplotlib.pyplot as plt
+
 '''
-Problem C.1 on 551 HW 4
+A study on on how p(t) is impacted by linear feedback parameters
 '''
 
 #
@@ -34,19 +35,16 @@ rho_ramp_up = LinearReactivityRamp(0,0.5 * d.beff, 1) # 0$ -> 0.5$ in 1s
 rho_ramp_down = LinearReactivityRamp(0.5 * d.beff, 0, 5) #0.5$ -> 0$ in 5 s
 rho = PieceWiseReactivityRamp(times , [rho_ramp_up, rho_ramp_down], t)
 
-# compare feedback numerical w/ analytic
-solver = Solver(data,t,rho, debug=False)
-solver.solve(0.5,feedback=True)
-power_feed_solver = solver.p
-power_analytic = solver.analyticPower1DG()
-
-
-# plot analytic comparison
+#compare feedback
 p = Plotter(t)
-p.addData(power_analytic, label="Analytic", marker="--")
-p.addData(power_analytic, label="EPK", marker="x")
-p.save("./results/p1.pdf")
 
-p = Plotter(t, ylabel=r"$\rho$ [\$]")
-p.plotReactivityRamp(rho,data.beff)
-p.save("./results/p1_rx.pdf")
+fig, ax = plt.subplots(1,1)
+cs = ["k", "b", "r", "purple", "g"]
+for i, gamma_D in enumerate([0, .3, 1., 2., 6]):
+    solver = Solver(data, t, rho)
+    solver.d.lambda_H += 1
+    solver.d.gamma_D += -gamma_D
+    solver.solve(0.5, feedback=True)
+    p.addData(solver.p, label=r"$\gamma_d$ = %.1f"%gamma_D, marker=cs[i])
+
+p.save("./results/gamma_study.pdf")
