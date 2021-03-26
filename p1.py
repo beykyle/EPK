ac6c063f99fcb1f7092dad7214d61d921d967554
deanrp <deanrp@highpoint.engin.umich.edu>
2021-03-26 02:02:22 -0400

    made demo for solvers

--- p1.py
+++ p1.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 test()
 
 # set up time grid
-timesteps = 151
+timesteps = 4200
 t = np.linspace(0,6.0001,num=timesteps)
 
 # set up kinetics data
@@ -31,27 +31,35 @@ rho_ramp_up = LinearReactivityRamp(0,0.5 * d.beff, 1) # 0$ -> 0.5$ in 1s
 rho_ramp_down = LinearReactivityRamp(0.5 * d.beff, 0, 5) #0.5$ -> 0$ in 5 s
 rho = PieceWiseReactivityRamp(times , [rho_ramp_up, rho_ramp_down], t)
 
-# run the solver
+# -- demo
+# compare no feedback
 solver = Solver(data,t,rho, debug=False)
-data.gamma_D += -1.2 #changing these for development
-data.lambda_H += 1 #changing these for development
+solver.solve(0.5, False)
+power_nofeed_solver = solver.p
 solver.solve(0.5, True)
-
-exit()
-
-
-power_numeric = solver.p
-# get the analytic solution
+power_feed_solver = solver.p
 power_analytic = solver.analyticPower1DG()
 
-# plot reactivity ramp
-plot_rx = Plotter(t, ylabel=r"$\rho$ [\$]")
-plot_rx.plotReactivityRamp(rho, data.beff)
-#plot_rx.save("./results/p1_rx.pdf")
+fig, ax = plt.subplots(1,1)
+ax.plot(t, power_analytic, "k-", label = "analytic")
+ax.plot(t, power_nofeed_solver, "rX", alpha = .02, label = "no feedback solver")
+ax.plot(t, power_feed_solver, "b.", alpha = .02, label = "feedback solver")
+ax.legend()
+ax.set_xlabel("t")
+ax.set_ylabel("p")
 
-# compare analytic and numeric power with the plotter
-plotter = Plotter(t)
-plotter.addData(power_analytic, label="Analytic", marker="--")
-plotter.addData(power_numeric,  label="EPKE Solver", marker="x")
-#plotter.save("./results/p1.pdf")
+#compare feedback
+fig, ax = plt.subplots(1,1)
+cs = ["k", "b", "r", "purple", "g"]
+for i, gamma_D in enumerate([0, .3, 1., 2., 6]):
+    solver = Solver(data, t, rho)
+    solver.d.lambda_H += 1
+    solver.d.gamma_D += -gamma_D
+
+    solver.solve(0.5, True)
+    ax.plot(t, solver.p, cs[i] ,label = r"$\gamma_d$ = %.1f"%gamma_D)
+ax.legend()
+ax.set_xlabel("t")
+ax.set_ylabel("p")
+
 plt.show()
