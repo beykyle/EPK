3c9a56b8d0ec2e0a9a98cdf49df4c110bd790ced
beykyle <beykyle@umich.edu>
2021-03-27 13:58:18 -0400

    Fix beta, better time steps for time step study, plot in ms

--- p1.py
+++ p1.py
@@ -21,7 +21,7 @@ d.mgt = 2.6E-15
 
 # precursor data
 d.lambda_precursor  = np.array([0.49405])
-d.beff              = np.array([0.76])
+d.beff              = np.array([0.0076])
 
 # thermal feedback dynamics data
 d.lambda_H = 0
@@ -36,37 +36,48 @@ rho_ramp_up = LinearReactivityRamp(0,0.5 * d.beff, 1) # 0$ -> 0.5$ in 1s
 rho_ramp_down = LinearReactivityRamp(0.5 * d.beff, 0, 5) #0.5$ -> 0$ in 5 s
 rho = PieceWiseReactivityRamp(times , [rho_ramp_up, rho_ramp_down], t)
 
-# set up plotter and solver, get analytic soln
+# set up solver
 solver = Solver(data,t,rho)
-power_analytic = solver.analyticPower1DG()
+
+# set up plotters
 power_plotter = Plotter(t)
+react_plotter = Plotter(t)
+precursor_plotter = Plotter(t, ylabel=r"$\zeta(t)$ [1/s]")
 
 # compare numerical w/ analytic, theta study
 solver.solve(0.5)
-power_plotter.addData(solver.p, label=r"EPK, $\theta=\frac{1}{2}$", marker=".")
+power_plotter.addData(solver.p, label=r"EPK, $\theta=\frac{1}{2}$", marker="k.")
+precursor_plotter.addData(solver.zetas[0,:], label=r"EPK, $\theta=\frac{1}{2}$", marker="k.")
+solver.reset()
 solver.solve(1)
-power_plotter.addData(solver.p, label=r"EPK, $\theta=1$", marker=".", alpha=0.5)
+power_plotter.addData(solver.p, label=r"EPK, $\theta=1$", marker="y.", alpha=0.3)
+precursor_plotter.addData(solver.zetas[0,:], label=r"EPK, $\theta=1$", marker="y.", alpha=0.3)
+solver.reset()
 #solver.debug = True
-solver.solve(0)
-power_plotter.addData(solver.p, label=r"EPK, $\theta=0$", marker=".", alpha=0.5)
+#solver.solve(0)
+#power_plotter.addData(solver.p, label=r"EPK, $\theta=0$", marker=".", alpha=0.5)
+#precursor_plotter.addData(solver.zetas[0,:], label=r"EPK, $\theta=0$", marker=".", alpha=0.5)
 
 # plot analytic soln on top
+power_analytic = solver.analyticPower1DG()
 power_plotter.addData(power_analytic, label="Analytic", marker="--")
 
 # plot reactivity
-react_plotter = Plotter(t)
 react_plotter.plotReactivityRamp(rho, data.beff)
 
 # save figures
-power_plotter.save("./results/theta_study_nofeedback.pdf")
+power_plotter.save("./results/theta_study.pdf")
 react_plotter.save("./results/p1_rx.pdf")
+precursor_plotter.save("./results/theta_study_zeta.pdf")
 
+#
 # time step study
-timesteps = [6001, 7001, 8001, 9001, 10001, 11001, 12001, 13001, 14001, 15001]
+timesteps = np.logspace(2,6, num=15)
 l2_diff_CN = []
 l2_diff_IMP = []
 RDM_diff_CN = []
 RDM_diff_IMP = []
+
 def L2(a,b):
     return np.linalg.norm(a-b) / np.linalg.norm(a)
 
@@ -92,12 +103,13 @@ for num_t in timesteps:
     solver.solve(1.0)
     solver.reset()
 
-p = Plotter(6.0/np.array(timesteps),  xlabel=r"time step [s]", ylabel=r"$(L_2(p_{analytic} -  p_{epk}))/L_2(p_{analaytic})$")
+p = Plotter(6.0E3/timesteps,  xlabel=r"time step [ms]", ylabel=r"$(L_2(p_{analytic} -  p_{epk}))/L_2(p_{analaytic})$")
 p.addData(np.array(l2_diff_CN), marker="kx", label="Crank-Nicholson")
 p.addData(np.array(l2_diff_IMP), marker="yx", label="Implicit", alpha=0.4)
+p.addData(np.ones((len(RDM_diff_CN),)) * 0.01, label="0.01")
 p.save("./results/time_step.pdf")
 
-p = Plotter(6.0/np.array(timesteps),  xlabel=r"time step [s]", ylabel=r"$(p^{max}_{analytic} -  p^{max}_{epk})/p^{max}_{analaytic}$")
+p = Plotter(6.0E3/timesteps,  xlabel=r"time step [ms]", ylabel=r"$(p^{max}_{analytic} -  p^{max}_{epk})/p^{max}_{analaytic}$")
 p.addData(np.array(RDM_diff_CN), marker="kx", label="Crank-Nicholson")
 p.addData(np.array(RDM_diff_IMP), marker="yx",label="Implicit", alpha=0.4)
 p.addData(np.ones((len(RDM_diff_CN),)) * 0.01, label="0.01")
