ac02b193f4f23849c4a671da222e1d9f7e6dc4f3
beykyle <beykyle@umich.edu>
2021-03-30 00:41:55 -0400

    Use corrected I.C., plot zetas and rho, use correct rho_IM

--- p4.py
+++ p4.py
@@ -12,9 +12,10 @@ Problem D.2 on 551 HW 4
 
 test()
 
+plt_logx=False
+
 #load in PARCS data
 parcs_data = pd.read_csv("PARCS.dat", sep = " ")
-
 
 
 def interp(qty, tt):
@@ -23,7 +24,7 @@ def interp(qty, tt):
 
 #set up time mesh
 start_time, end_time = parcs_data["Time"].iloc[0], parcs_data["Time"].iloc[-1]
-dt = 0.1e-3
+dt = 1.0e-5
 t = np.arange(start_time, end_time + dt, dt)
 
 t = t[:t.size//200]
@@ -46,26 +47,46 @@ beff = np.array([0.02584, 0.152, 0.13908, 0.30704, 0.1102, 0.02584])/100
 #create Data instance
 d = ConstantKineticsData()
 d.beff = beff
+d.gamma_D = -1.2*np.sum(beff)
+d.lambda_H = 1.0
+d.mgt = 2.6E-15
 d.lambda_precursor = lambda_precursor
 data = Data.buildFromConstant(d, t)
 data.mgt = interp("Generation-Time", t)
 data.f_fp = interp("Normalization-Factor", t)
+data.gamma_D = data.gamma_D/(data.f_fp )
 
 #create Rho instance
 rho = ReactivityGrid(t, interp("Reactivity", t))
-rho.rho = (np.ones_like(rho.rho) * 1.078) *np.sum(beff) # convert form $ to reactivity
-rho.rho[:100] = 0
+rho_PARCS = np.copy(rho.rho)
+idx = np.argmax(rho.rho)
+max_rho =  1.078 * np.sum(beff)
+rho.rho *= max_rho/ rho.rho[idx]
+rho.rho[idx:] = max_rho
+rx_plt = Plotter(t, ylabel=r"$\rho(t)$ [\$]")
+rx_plt.addData(rho.rho / np.sum(beff), logx=plt_logx, label=r"$\rho_{IM}$ [\$]")
 
 #create the solver instance
-solver = Solver(data, t, rho, debug=False)
-solver.d.lambda_H += 1.
-solver.d.gamma_D += -1.2
-solver.d.f_fp = interp("Normalization-Factor", t)
+solver = Solver(data, t, rho, debug=True)
+p0 = interp("Relative-Power",t)[0]
+solver.setIniCond(p0)
+#import pdb
+#pdb.run('solver.solve(0.5, feedback=True)')
 solver.solve(0.5, feedback=True)
 power_plt = Plotter(t)
-power_plt.addData(interp("Relative-Power",t) , label="PARCS", logx = True)
-power_plt.addData(solver.p*interp("Normalization-Factor", t), label="EPK", logx = True)
-power_plt.save("./results/parcs_d1.pdf")
+zeta_plt = Plotter(t, ylabel=r"$\frac{\zeta(t)}{\zeta(0)}$ [a.u.]")
 
-plt.show()
+power_plt.addData(interp("Relative-Power",t) , label="PARCS", logx=plt_logx)
+power_plt.addData(solver.p*interp("Normalization-Factor", t), label="EPK", logx=plt_logx)
+power_plt.save("./results/d2a_power.pdf")
+
+rx_plt.addData(solver.rho / np.sum(beff), logx=plt_logx, label=r"$EPK, \rho$ [\$]")
+rx_plt.addData(rho_PARCS, logx=plt_logx, label=r"$PARCS, \rho$ [\$]")
+rx_plt.save("./results/d2a_rx.pdf")
+
+for i in range(len(beff)):
+    zeta_plt.addData(np.copy(solver.zetas[i,:]/solver.zetas[i,0]), label="Group {}".format(i+1))
+zeta_plt.save("./results/c1b_zeta.pdf")
+
 plt.legend()
+plt.show()
